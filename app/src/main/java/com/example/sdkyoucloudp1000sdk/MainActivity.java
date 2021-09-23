fdc3dd1ba7b4ffc0764feaca864c6862245d119d
sil-dev <84126087+sil-dev@users.noreply.github.com>
2021-09-24 00:21:59 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/sdkyoucloudp1000sdk/MainActivity.java
+++ app/src/main/java/com/example/sdkyoucloudp1000sdk/MainActivity.java
@@ -167,6 +167,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    
+     @Override
+    protected void onStart()
+    { super.onStart();
+    Log.d("lifecycle","onStart invoked");
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        moveTaskToBack(true);
+    }
+
+    @Override
+    protected void onResume()
+    { super.onResume();
+    Log.d("lifecycle","onResume invoked");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d("lifecycle","onPause invoked");
+    }
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
+    }
+
 
 
 
